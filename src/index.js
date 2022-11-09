e46e22725488a81438e008a6a643f245a223c030
Akalanka <akalankaperera128@gmail.com>
2022-11-09 21:11:24 +0530

    CI: Initial release

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
+import { mapValue, replaceOperator } from "./utils";
+
+const complexOperators = ['and', 'or'];
+
 const mongooseFilterQuery = (req, res, next) => {
-  if (req.query.filter) {
-    Object.keys(req.query.filter).forEach((key) => {
-      // TODO: implement logic
-    })
-  }
-  if (req.query.sort) {
-    Object.keys(req.query.sort).forEach((key) => {
-      const dir = req.query.sort[key]
-      if (dir === '1' || dir === '-1') {
-        req.query.sort[key] = parseInt(dir)
-      }
-    })
+  try {
+    if (req.query.filter) {
+      Object.keys(req.query.filter).forEach((key) => {
+        const value = req.query.filter[key];
+        const complexOp = complexOperators.find((op) => value.startsWith(`${op}(`));
+        if (complexOp) {
+          const values = replaceOperator(value, complexOp)?.split(',');
+          req.query.filter[`$${complexOp}`] = values.map((subValue) => ({ [key]: mapValue(subValue) }));
+          delete req.query.filter[key];
+        } else {
+          req.query.filter[key] = mapValue(value);
+        }
+      })
+    }
+    if (req.query.sort) {
+      Object.keys(req.query.sort).forEach((key) => {
+        const dir = req.query.sort[key]
+        if (dir === '1' || dir === '-1') {
+          req.query.sort[key] = parseInt(dir)
+        }
+      })
+    }
+  } catch (e) {
+    console.error('[ Mongoose-FilterQuery ] - Failed to parse filters from query', e)
   }
   next();
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,26 @@
 import mongooseFilterQuery from '../src';
+import * as utils from '../src/utils';
+import { basicFilterReq, basicFilterResult, complexFilterReq, complexFilterResult, sortsReq, sortResult, req } from './__mocks';
 
 describe("test mongoose-filter-query", () => {
-    test("test-1", async () => {
-       
+    test("test-basic-filters", async () => {
+        mongooseFilterQuery(basicFilterReq, {}, () => { });
+        expect(basicFilterReq.query.filter).toEqual(basicFilterResult);
+    });
+    test("test-complex-filters", async () => {
+        mongooseFilterQuery(complexFilterReq, {}, () => { });
+        expect(complexFilterReq.query.filter).toEqual(complexFilterResult);
+    });
+    test("test-sortsReq", async () => {
+        mongooseFilterQuery(sortsReq, {}, () => { });
+        expect(sortsReq.query.sort).toEqual(sortResult);
+    });
+    test("handle-error", async () => {
+        jest.spyOn(utils, 'mapValue').mockImplementation(() => {
+            throw new Error('test-error');
+        });
+        const next = jest.fn();
+        mongooseFilterQuery(req, {}, next);
+        expect(next).toHaveBeenCalled();
     });
 });
